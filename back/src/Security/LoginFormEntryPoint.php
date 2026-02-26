e752c9dd55ac6f356144ce0f5e5550b632e822eb
TURPIN Jonathan <turpin.j@hotmail.fr>
2026-02-26 09:26:18 +0100

    login form on api platform docs

--- back/src/Security/LoginFormEntryPoint.php
+++ back/src/Security/LoginFormEntryPoint.php
@@ -7,11 +7,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 final class LoginFormEntryPoint implements AuthenticationEntryPointInterface
 {
+    use TargetPathTrait;
+
     public function start(Request $request, ?AuthenticationException $authException = null): Response
     {
+        // Save target path under 'main' so form_login redirects back after login
+        if ($request->hasSession()) {
+            $this->saveTargetPath($request->getSession(), 'main', $request->getUri());
+        }
+
         return new RedirectResponse('/login');
     }
 }
